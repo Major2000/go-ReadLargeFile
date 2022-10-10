2682f48d11e3cdd1f4dd6e248c01549deb53d484
Major2000 <majormadobe@gmail.com>
2022-10-10 13:35:45 +0300

    func: Process

--- readLarge.go
+++ readLarge.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -23,7 +26,7 @@ func main() {
 	file, err := os.Open(fileName)
 
 	if err != nil {
-		fmt.Println("cannot able to read file", err)
+		fmt.Println("Cannot read file", err)
 		return
 	}
 
@@ -31,13 +34,19 @@ func main() {
 
 	queryStartTime, err := time.Parse("200601-02T15:04:05.0000Z", finishTimeArg)
 	if err != nil {
-		fmt.Println("could not parse the finish time", finishTimeArg)
+		fmt.Println("Could not parse the finish time", finishTimeArg)
+		return
+	}
+
+	queryFinishTime, err := time.Parse("2006-01-02T15:04:05.0000Z", finishTimeArg)
+	if err != nil {
+		fmt.Println("Could not parse the finish time", finishTimeArg)
 		return
 	}
 
 	filestat, err := file.Stat()
 	if err != nil {
-		fmt.Println("could ot get the file stat")
+		fmt.Println("Could not get the file stat")
 		return
 	}
 
@@ -65,7 +74,7 @@ func main() {
 	_, err = file.ReadAt(lastLine, offset+1)
 
 	if err != nil {
-		fmt.Println("could not read the last line with offset", offset, "and last line size", lastLineSize)
+		fmt.Println("Could not read the last line with offset", offset, "and last line size", lastLineSize)
 		return
 	}
 
@@ -73,5 +82,64 @@ func main() {
 	logCreationTimeString := logSlice[0]
 
 	lastLogCreationTime, err := time.Parse("2006-01-02T15:04:05.0000Z", logCreationTimeString)
+	if err != nil {
+		fmt.Println("Cannot parse time :", err)
+	}
 
+	if lastLogCreationTime.After(queryStartTime) && lastLogCreationTime.Before(queryFinishTime) {
+		Process(file, queryStartTime, queryFinishTime)
+	}
+
+	fmt.Println("\nTime taken -", time.Since(s))
+
+}
+
+func Process(f *os.File, start time.Time, end time.Time) error {
+
+	linesPool := sync.Pool{New: func() interface{} {
+		lines := make([]byte, 250*1024)
+		return lines
+	}}
+
+	stringPool := sync.Pool{New: func() interface{} {
+		lines := ""
+		return lines
+	}}
+
+	r := bufio.NewReader(f)
+
+	var wg sync.WaitGroup
+
+	for {
+		buf := linesPool.Get().([]byte)
+
+		n, err := r.Read(buf)
+		buf = buf[:n]
+
+		if n == 0 {
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			if err == io.EOF {
+				break
+			}
+			return err
+		}
+
+		nextUntilNewline, err := r.ReadBytes('\n')
+
+		if err != io.EOF {
+			buf = append(buf, nextUntilNewline...)
+		}
+
+		wg.Add(1)
+		go func() {
+			ProcessChunck(buf, &linesPool, &stringPool, start, end)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	return nil
 }
